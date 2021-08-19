f5779556fb5cbedb21b6139fd11c196f858dfee3
LeFodeurCou <lefodeurcou@gmail.com>
2021-08-19 18:02:35 +0200

    Local storage used for theme choice - open graph meta tags done

--- build/lefodeurcou.local/public_html/_dev/js/toggler.js
+++ build/lefodeurcou.local/public_html/_dev/js/toggler.js
@@ -4,19 +4,32 @@ export const themeToggler = () => {
 	let day = false;
 	if (themeToggler)
 		themeToggler.addEventListener(
-		'click',
-		e => {
-			const body = document.querySelector('body');
-			if (body)
-				body.classList.toggle('light-theme');
-			const darkOrLight = themeToggler.querySelector('.jsRotate');
-			if (darkOrLight)
-			{
-				day = !day;
-				darkOrLight.style.transform = 'rotate(' + (day ? '30deg' : '-30deg') + ')';
+			'click',
+			e => {
+				const body = document.querySelector('body');
+				if (body)
+					body.classList.toggle('light-theme');
+				const darkOrLight = themeToggler.querySelector('.jsRotate');
+				if (darkOrLight)
+				{
+					day = !day;
+					darkOrLight.style.transform = 'rotate(' + (day ? '30deg' : '-30deg') + ')';
+					localStorage.setItem("toggleTheme", day);
+				}
 			}
-
-			
-		}
 		)
-	};
+	const toggleTheme = localStorage.getItem('toggleTheme');
+	if (toggleTheme === "true")
+	{
+		const body = document.querySelector('body');
+		if (body)
+			body.classList.toggle('light-theme');
+		const darkOrLight = themeToggler.querySelector('.jsRotate');
+		if (darkOrLight)
+		{
+			day = !day;
+			darkOrLight.style.transform = 'rotate(' + (day ? '30deg' : '-30deg') + ')';
+			localStorage.setItem("toggleTheme", day);
+		}
+	}
+};
